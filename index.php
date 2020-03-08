f485082764d0792fe8029d9c670d6ccb04b334df
Cloud-Panda <abbyliguma@gmail.com>
2020-03-08 17:49:48 +0200

    landing page font,background & images changed

--- index.php
+++ index.php
@@ -7,47 +7,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="css/style.css">
     <!--google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Antic+Didone|Cinzel|Cinzel+Decorative|Cormorant+SC|DM+Serif+Display|Dawning+of+a+New+Day|IM+Fell+French+Canon|Italiana|Meddon|Mrs+Saint+Delafield|Nothing+You+Could+Do|Playfair+Display+SC|Prata|Reenie+Beanie|Roboto+Mono|Seaweed+Script|Source+Code+Pro|Suravaram|Vollkorn+SC|Zilla+Slab+Highlight&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css?family=Cinzel|Cinzel+Decorative" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Caladea&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dosis&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|EB+Garamond|Libre+Baskerville|Playfair+Display&display=swap" rel="stylesheet">
   </head>
   <body>
-    <!--JS script tags -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js" integrity="sha256-ABrLsV2caVEMCBfm3eNhv/CYQG+tGCqzw2f4b/Pag0M=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TimelineMax.min.js" integrity="sha256-v/YsEqWkVsiNHryCWg+vjC/eDBakMAgOsCJRa0kNPOg=" crossorigin="anonymous"></script>
-    <script src="scripts/animation.js"></script>
-  <header>
-      <section>
+    
+      <header>
 
         <div class="container">
+
       <!--right section-->
-
-
       <div class="image-section">
-        <div class="hero">
-          <img src="./images/6.jpg">
-          <h1 class="headline">VOGUE</h1>
-        </div>
-        <div class="slider"></div>
+        <h1>VOGUE</h1>
+        <img src="./images/30.jpg">
+        <p><i> “I like my money right where I can see it: <br>hanging in my closet.” </i></p> 
      </div>
 
       <!--left section-->
       <div class="btn-section">
       <h1>The World Of Fashion</h1>
-        <p>Do you think you know the fashion world?</p>
-        <hr>
+        <p>Ready to test your fashion knowledge?</p>
+        <hr id="btn-hr"/>
         <form action="process.php">
-            <button>Sart Quiz</button>
+            <button><b>Sart Quiz</b></button>
         </form>
       </div> 
 
-      <!--about-section-->
       <div class="about-section">
-        <img src="./images/22.jpg" id="about-img">
+        <img src="./images/37.jpg" id="about-img">
       </div>
 
-         </div>
-        </section>
-    
-</section>
+      </div>
+      </header>
+   
   </body>
 </html>
